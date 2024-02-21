ee2cc35b3358081a793d3fbef630680054165fd0
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-02-21 09:25:17 -0300

    clonacion del proyecto de Emi

---
+++ src/components/pages/Administrador.jsx
@@ -0,0 +1,52 @@
+import { Table } from "react-bootstrap";
+import ItemProducto from "./producto/ItemProducto";
+import { useEffect, useState } from "react";
+import { leerProductosAPI } from "../../helpers/queries";
+import {Link} from 'react-router-dom'
+const Administrador = () => {
+const [productos, setProductos] =  useState([]);
+
+useEffect(  ()=>{
+  consultarAPI()
+}, [])
+
+const consultarAPI = async()=>{
+  try{
+    const respuesta = await leerProductosAPI();
+    setProductos(respuesta);
+  }catch (error){
+    console.log(error)
+  }
+}
+
+  return (
+    <section className="container mainSection">
+      <div className="d-flex justify-content-between align-items-center mt-5">
+        <h1 className="display-4 ">Productos disponibles</h1>
+        <Link className="btn btn-primary" to='/administrador/crear'>
+          <i className="bi bi-file-earmark-plus"></i>
+        </Link>
+      </div>
+      <hr />
+      <Table responsive striped bordered hover>
+        <thead>
+          <tr className="text-center">
+            <th>Cod</th>
+            <th>Producto</th>
+            <th>Precio</th>
+            <th>URL de Imagen</th>
+            <th>Categoria</th>
+            <th>Opciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            productos?.map((producto)=>  <ItemProducto key={producto.id} producto={producto}></ItemProducto>)
+          }
+        </tbody>
+      </Table>
+    </section>
+  );
+};
+
+export default Administrador;
