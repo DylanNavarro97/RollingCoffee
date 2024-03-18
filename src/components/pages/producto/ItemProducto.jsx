fb80d3800d4cd9f9730aca0f3d9c68439d5f9a44
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-03-18 10:26:33 -0300

    correcciones para realizar conexion con la nueva BD de Mongo

--- src/components/pages/producto/ItemProducto.jsx
+++ src/components/pages/producto/ItemProducto.jsx
@@ -16,7 +16,7 @@ const ItemProducto = ({producto, setProductos}) => {
       cancelButtonText: "Cancelar"
     }).then( async (result) => {
       if (result.isConfirmed) {
-        const respuesta = await borrarProductoAPI(producto.id)
+        const respuesta = await borrarProductoAPI(producto._id)
         if (respuesta.status === 200){
           Swal.fire({
             title: "Producto eliminado",
@@ -40,9 +40,9 @@ const ItemProducto = ({producto, setProductos}) => {
 
   return (
     <tr>
-      <td className="text-center">{producto.id}</td>
+      {/* <td className="text-center">{producto._id}</td> */}
       <td>{producto.nombreProducto}</td>
-      <td className="text-end">${producto.precioProducto}</td>
+      <td className="text-end">${producto.precio}</td>
       <td className="text-center td-img">
         <img
           src={producto.imagen}
@@ -52,7 +52,7 @@ const ItemProducto = ({producto, setProductos}) => {
       </td>
       <td>{producto.categoria}</td>
       <td className="text-center">
-        <Link className="me-md-2 mb-2 mb-md-0 btn btn-warning" to={`/administrador/editar/${producto.id}`}>
+        <Link className="me-md-2 mb-2 mb-md-0 btn btn-warning" to={`/administrador/editar/${producto._id}`}>
           <i className="bi bi-pencil-square" to="/editar"></i>
         </Link>
         <Button variant="danger" onClick={borrarProducto}>
